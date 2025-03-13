913cd4a77b69f88f122c473f363e5e6c884b84a5
AbbieF-ctrl <abigail.foggarthy25@compscihigh.org>
2025-03-13 18:07:16 +0000

    Did the code along

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 // import your mongoose
 
 // install your mongoose
-
+const mongoose = require("mongoose");
 const app = express();
 
 app.use((req, res, next) => {
@@ -12,9 +12,31 @@ app.use((req, res, next) => {
 
 // create a studentSchema with name, grade, advisory, and fav subject
 
+const studentSchema = new mongoose.Schema({
+  name: {type: String, required: true},
+  grade: {type: Number, default: 9},
+  favSubject: {type: String, required: true}
+})
+
 // connect your schema to a model called Student
 
+const Student = mongoose.model("Student", studentSchema, "Students")
+
 // create a route hanlder for /g12 that returns every student in grade 12
+
+app.get("/g12", async (req, res) => {
+const students = await Student.find({ })
+res.json(students)
+})
+
+app.post('/student/save', async (req, res) => {
+  const me = await new Student({
+    name: req.body.name,
+    grade: req.body.grade,
+    favSubject: req.body.favSubject
+  }).save()
+  res.json(student)
+})
 
 // (OYO) create a route hanlder for /me that returns yourself without using your name in the query
 
@@ -27,6 +49,33 @@ app.use((req, res, next) => {
 // (OYO) save 2 more documents about students at your table
 // make sure to start your server 
 
+async function startServer(){
+await mongoose.connect("mongodb+srv://SE12AbiF:CSH2025@cluster0.ebb1f.mongodb.net/codealong?retryWrites=true&w=majority&appName=Cluster0")
+
+const me = await new Student({
+  name: "Castro",
+  grade: 13,
+  favSubject: "Art"
+}).save()
+
+const abi = await new Student({
+  name: "Abigail",
+  grade: 12,
+  favSubject: "Software Enginnering"
+}).save()
+
+const andry = await new Student({
+  name: "Andry",
+  grade: 11,
+  favSubject: "Music"
+}).save()
+
+app.listen(3000, () => {
+  console.log("Server is running")
+})
+}
+
+startServer()
 
 // call startServer
 
